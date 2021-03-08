4088c2a71f544ad4d3bc86fb90d4e513b332ad07
Govind <govindajithkumar@gmail.com>
2021-03-08 15:05:43 -0500

    Adding original and new header and main .cc files

---
+++ Orbslam2_Files_Original/Examples/ROS/ORB_SLAM2/build/gtest/googlemock/CMakeFiles/gmock.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gmock.dir/__/googletest/src/gtest-all.cc.o"
+  "CMakeFiles/gmock.dir/src/gmock-all.cc.o"
+  "/home/govind/orb_slam2_ws/src/ORB_SLAM2/Examples/ROS/ORB_SLAM2/lib/libgmock.pdb"
+  "/home/govind/orb_slam2_ws/src/ORB_SLAM2/Examples/ROS/ORB_SLAM2/lib/libgmock.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/gmock.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Orbslam2_Files_Original/Examples/ROS/ORB_SLAM2/build/gtest/googlemock/gtest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /usr/src/googletest/googletest
+# Build directory: /home/govind/orb_slam2_ws/src/ORB_SLAM2/Examples/ROS/ORB_SLAM2/build/gtest/googlemock/gtest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
