8ee45528b2f91e403da251031c381d0e4feff56c
Govind Ajith Kumar <50176400+govindak-umd@users.noreply.github.com>
2021-03-31 12:54:34 -0400

    Capture Video Code Clean up

--- capture_video.py
+++ capture_video.py
@@ -2,19 +2,19 @@ import cv2
 import time
 capture = cv2.VideoCapture(0)
 fourcc = cv2.VideoWriter_fourcc('X','V','I','D')
-videoWriter = cv2.VideoWriter(str(time.time())+'.avi', fourcc, 30.0, (640,480))
+videoWriter = cv2.VideoWriter(str(time.time())+'.avi', fourcc, 20.0, (640,480))
 print('Capturing Video now ...')
 
 while (True):
-	print('Capturing Video now ...')
-	ret, frame = capture.read()
-	 
-	if ret:
-	    cv2.imshow('video', frame)
-	    videoWriter.write(frame)
+    print('Capturing Video now ...')
+    ret, frame = capture.read()
 
-	if cv2.waitKey(1) == 27:
-	    break
+    if ret:
+        cv2.imshow('video', frame)
+        videoWriter.write(frame)
+
+    if cv2.waitKey(1) == 27:
+        break
  
 capture.release()
 videoWriter.release()
