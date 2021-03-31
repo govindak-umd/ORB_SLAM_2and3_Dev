0e7a4c093d3715d7d05a6d87b2dc3ffbaacce296
Govind Ajith Kumar <50176400+govindak-umd@users.noreply.github.com>
2021-03-31 07:29:15 -0400

    Update capture_video.py

--- capture_video.py
+++ capture_video.py
@@ -2,7 +2,7 @@ import cv2
 import time
 capture = cv2.VideoCapture(0)
 fourcc = cv2.VideoWriter_fourcc('X','V','I','D')
-videoWriter = cv2.VideoWriter(str(time.time())+'.avi', fourcc, 30.0, (640,480))
+videoWriter = cv2.VideoWriter(str(time.time())+'.avi', fourcc, 20.0, (640,480))
 print('Capturing Video now ...')
 
 while (True):
